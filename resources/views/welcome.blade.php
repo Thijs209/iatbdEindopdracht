f1aaaa16d8552f85933f6c7adecea1588ecb3ea2
Thijs209 <s1128756@student.hsleiden.nl>
2022-06-13 16:49:17 +0200

    small changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,13 @@
     </section>
     <section class="homepage__main">
         <article class="homepage__upload-pet">
-            <h1 class="homepage__title">Zoek iemand om op je huisdier te passen</h1>
-            <p class="homepage__text">Upload wat foto`s en informatie over je huisdier en vind in no time een oppas.</p>
+            <h1 class="homepage__title">Zoek een oppas voor je huisdier</h1>
+            <p class="homepage__text">Upload een aantal leuke foto`s en informatie over je huisdier en vind in no time een oppas die goed bij jou dier past!</p>
             <button class="homepage__button upload-button" onclick="window.location='/petprofile'">Zoek een oppasser</button>
         </article>
         <article class="homepage__create-account">
             <h1 class="homepage__title">Word een dieren Op Passer</h1>
-            <p class="homepage__text">Laat wat foto`s van jou of je huis zien, vertel wat over jezelf en verdien een extra zakcentje door gewoon op wat dieren te passen.</p>
+            <p class="homepage__text">Laat wat foto`s van jou of je huis zien, vertel wat over jezelf en verdien een extra zakcentje tijdens het bezig zijn met dieren.</p>
             <button class="homepage__button" onclick="window.location='/account'">Wordt een oppasser</button>
         </article>
     </section>
